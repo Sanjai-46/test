d5bb6f5fbc57886d44b5daac75e534e92f21ddc0
Sanjai <poojan200102@gmail.com>
2024-06-17 10:58:25 +0530

    final commit

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
+require('dotenv').config();
 const admin = require("firebase-admin");
-const serviceAccount = require("./key/key.json"); // Your Firebase Admin SDK key
 const express = require("express");
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    "type": "service_account",
+    "project_id": process.env.GOOGLE_CLOUD_PROJECT_ID,
+    "private_key_id": process.env.GOOGLE_CLOUD_PRIVATE_KEY_ID,
+    "private_key": process.env.GOOGLE_CLOUD_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    "client_email": process.env.GOOGLE_CLOUD_CLIENT_EMAIL,
+    "client_id": process.env.GOOGLE_CLOUD_CLIENT_ID,
+    "auth_uri": process.env.GOOGLE_CLOUD_AUTH_URI,
+    "token_uri": process.env.GOOGLE_CLOUD_TOKEN_URI,
+    "auth_provider_x509_cert_url": process.env.GOOGLE_CLOUD_AUTH_PROVIDER_X509_CERT_URL,
+    "client_x509_cert_url": process.env.GOOGLE_CLOUD_CLIENT_X509_CERT_URL
+  }),
   databaseURL: "https://chats-enthu.firebaseio.com" // Replace with your Firebase database URL
 });
 
